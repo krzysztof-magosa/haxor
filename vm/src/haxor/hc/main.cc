56f3f40794027f98be178cf3d77e37a297996c85
Krzysztof Magosa <krzysztof@magosa.pl>
2016-01-31 22:07:52 +0100

    display message about invalid number of arguments

--- vm/src/haxor/hc/main.cc
+++ vm/src/haxor/hc/main.cc
@@ -1,6 +1,11 @@
 #include "haxor/hc/compiler.hh"
 
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: hc <source-to-compile.hax>" << std::endl;
+    return 1;
+  }
+
   haxor::compiler compiler;
   compiler.compile(argv[1]);
 
