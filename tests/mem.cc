3dbb28f8d2d80768c5b52ff3f9dc67cdb0d5620d
Krzysztof Magosa <krzysztof@magosa.pl>
2016-06-04 18:13:52 +0200

    next tests for mem

--- tests/mem.cc
+++ tests/mem.cc
@@ -33,3 +33,29 @@ TEST(mem_tests, read_word_range_2)
   mem.alloc(4096);
   EXPECT_THROW(mem.read_word(4096), haxor::mem_range_error);
 }
+
+TEST(mem_tests, write_string)
+{
+  haxor::mem mem;
+  haxor::word_t addr = 1024;
+
+  mem.alloc(4096);
+  mem.write_string(addr, "Haxor");
+
+  // strings in Haxor are also aligned to 8 bytes.
+  EXPECT_EQ('H', mem.read_word(addr));
+  EXPECT_EQ('a', mem.read_word(addr+=8));
+  EXPECT_EQ('x', mem.read_word(addr+=8));
+  EXPECT_EQ('o', mem.read_word(addr+=8));
+  EXPECT_EQ('r', mem.read_word(addr+=8));
+}
+
+TEST(mem_tests, read_string)
+{
+  haxor::mem mem;
+  mem.alloc(4096);
+  mem.write_string(2048, "Virtual");
+  const std::string result = mem.read_string(2048);
+
+  EXPECT_EQ("Virtual", result);
+}
