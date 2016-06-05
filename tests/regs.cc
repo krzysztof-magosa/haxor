84c77a34fcf5bfc32e2b3383e14b8b8e6cb5fd54
Krzysztof Magosa <krzysztof@magosa.pl>
2016-06-05 16:13:02 +0200

    expected value should be first

--- tests/regs.cc
+++ tests/regs.cc
@@ -5,7 +5,7 @@ TEST(regs_test, ignore_writes_to_zero)
 {
   haxor::regs regs;
   regs.write(0, 123); // write 123 to register 0
-  ASSERT_EQ(regs.read(0), 0); // still zero
+  ASSERT_EQ(0, regs.read(0)); // still zero
 }
 
 TEST(regs_test, read_write)
