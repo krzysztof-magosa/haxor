4b315dfa72555bc7b807402e2689d90638ca4f2a
Krzysztof Magosa <krzysztof@magosa.pl>
2016-06-05 16:12:33 +0200

    check read/write of regs

--- tests/regs.cc
+++ tests/regs.cc
@@ -7,3 +7,10 @@ TEST(regs_test, ignore_writes_to_zero)
   regs.write(0, 123); // write 123 to register 0
   ASSERT_EQ(regs.read(0), 0); // still zero
 }
+
+TEST(regs_test, read_write)
+{
+  haxor::regs regs;
+  regs.write(5, 123);
+  ASSERT_EQ(123, regs.read(5));
+}
